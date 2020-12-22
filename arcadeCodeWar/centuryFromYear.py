6804713db3d12448555c8eedb81127f10f80069b
m26unknw <shukla9915294376@gmail.com>
2020-12-22 15:05:19 +0530

    Added

---
+++ arcadeCodeWar/centuryFromYear.py
@@ -0,0 +1,10 @@
+def centuryFromYear(year):
+    if year%100==0:
+        return year/100
+    else:
+        return int(year/100)+1
+    
+
+
+print(centuryFromYear(1920))
+print(centuryFromYear(2010))
