994fc4ff4e71c0ed0306161f0586f0450ffd64a4
Monu Shukla <monyou1mail@gmail.com>
2021-01-19 11:10:20 +0530

    Added

--- Math/gcdOfNumber.py
+++ Math/gcdOfNumber.py
@@ -1,16 +1,16 @@
 # Finding gcd of two number
 
-m=20
-n=10
+def gcdOfTwoNumber(m,n):
+    while m%n!=0:
+        old_m=m
+        print(old_m)
+        old_n=n
+        print(old_n)
 
-while m%n!=0:
-    old_m=m
-    print(old_m)
-    old_n=n
-    print(old_n)
+        m=old_n
+        print(m)
+        n=old_m%old_n
+    return  n
 
-    m=old_n
-    print(m)
-    n=old_m%old_n
-print(n)
+print(gcdOfTwoNumber(20,10))
     
